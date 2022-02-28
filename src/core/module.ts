6486722c1d0f01be0c1974fd0c6465a703d873d3
VictorMadu <ebubevm@gmail.com>
2022-02-28 17:50:53 +0100

    Redesign of Module
    - Added interface
    - Redesigned module to be more flexible and follow the SOLID priniciple more

--- src/core/module.ts
+++ src/core/module.ts
@@ -9,7 +9,14 @@ import {
   IClassWithKey,
   IController,
   IHandlerMetaData,
+  IService,
 } from "./interface";
+
+interface IListeners {
+  onReady: ((fastify: FastifyInstance) => void)[];
+  onStart: (() => void)[];
+  onClose: (() => void)[];
+}
 
 export type IConfig = {
   imports: ReturnType<ReturnType<typeof Module>>[];
@@ -18,113 +25,108 @@ export type IConfig = {
   exports: IServiceDecoConstructor[];
 };
 
+interface IServiceAdapter {
+  attachLifeCycleListener: (service: IService) => void;
+}
+
+interface IControllerAdapter {
+  attachToRoute: (routeConfig: {}, baseConfig: { basePath: string }) => void;
+}
+
 export function Module(config: IConfig) {
   return function (
     Target: IControllerDecoConstructor | IServiceDecoConstructor
-  ): { load: (fastify: FastifyInstance) => Container } {
+  ): any {
     return class Decorated extends Target {
-      private static localContainer: Container;
+      private localContainer: Container;
       private static exportContainer: Container;
-      private static controllerContainer: Container;
-      constructor(fastify: FastifyInstance) {
+      private controllerContainer: Container;
+
+      constructor(
+        private serviceAdapter: IServiceAdapter,
+        private controllerAdapter: IControllerAdapter
+      ) {
         super();
-        Decorated.localContainer = this.createContainer();
+        this.localContainer = this.createContainer();
         Decorated.exportContainer = this.createContainer();
-        Decorated.controllerContainer = this.createContainer();
-        this.loadAction(fastify);
+        this.controllerContainer = this.createContainer();
+        this.loadAction();
       }
 
       // PUBLIC: The only public method
-      static load(fastify: FastifyInstance) {
+      static load(
+        serviceAdapter: IServiceAdapter,
+        controllerAdapter: IControllerAdapter
+      ) {
         // if not localContainer or exportContainer or controllerContainer
-        if (!Decorated.exportContainer) new Decorated(fastify);
+        if (!Decorated.exportContainer)
+          new Decorated(serviceAdapter, controllerAdapter);
         return Decorated.exportContainer;
       }
 
-      private loadAction(fastify: FastifyInstance) {
-        this.loadImportModules(fastify);
+      private loadAction() {
+        const importContainers = this.getImportContainers();
         this.bindInjectables(
-          [this.getAllLocalServices(), Decorated.localContainer],
+          [this.getAllLocalServices(), this.localContainer],
           [config.exports, Decorated.exportContainer],
-          [config.controllers, Decorated.controllerContainer]
+          [config.controllers, this.controllerContainer]
         );
-        this.loadServices(fastify);
-        this.loadControllers(fastify);
+        const mergedContainers = this.getMergedContainers(importContainers);
+        this.loadServices(mergedContainers);
+        this.loadControllers(mergedContainers);
       }
 
-      private loadImportModules(fastify: FastifyInstance) {
-        _.forEach(config.imports, (importModules) => {
-          importModules.load(fastify);
-        });
+      private getMergedContainers(importContainers: Container[]) {
+        return Container.merge(
+          this.localContainer,
+          Decorated.exportContainer,
+          this.controllerContainer,
+          ...importContainers
+        ) as Container;
       }
 
-      private loadControllers(fastify: FastifyInstance) {
+      private getImportContainers(): Container[] {
+        return _.map(config.imports, (importModules) =>
+          importModules.load(this.serviceAdapter, this.controllerAdapter)
+        );
+      }
+
+      private loadControllers(container: Container) {
         const resolvedControllers = _.map(config.controllers, (controller) =>
-          this.getInstanceFromContainer(
-            this.getMergedContainer(fastify, true),
-            controller
-          )
+          this.getInstanceFromContainer(container, controller)
         );
 
         return _.map(resolvedControllers, (controller) => {
           const controllerMethodsName = getAllClassMethodsName(
             controller.constructor
           );
-          return _.map(controllerMethodsName, (methodName) =>
-            this.registerControllerHandler(fastify, controller, methodName)
-          );
+          return _.forEach(controllerMethodsName, (methodName) => {
+            if (methodName === "$METADATA")
+              return throwError(
+                `methodName of controller ${controller} should not be used since it is a resevered key`
+              );
+
+            const handlerWithMetaData = controller[methodName];
+            this.controllerAdapter.attachToRoute(
+              {
+                ...handlerWithMetaData.$METADATA,
+                handler: this.getControllerHandlerFn(handlerWithMetaData),
+              },
+              { basePath: controller.$METADATA.basePath }
+            );
+            return;
+          });
         });
       }
 
-      private loadServices(fastify: FastifyInstance) {
-        const container = this.getMergedContainer(fastify);
+      private loadServices(container: Container) {
         return _.map(config.services, (service) => {
           const serviceInstance = this.getInstanceFromContainer(
             container,
             service
           );
-
-          this.attachLifeCycleListeners(fastify, serviceInstance);
+          this.serviceAdapter.attachLifeCycleListener(serviceInstance);
         });
-      }
-
-      private attachLifeCycleListeners(
-        fastify: FastifyInstance,
-        service: ConstructorReturnType<IServiceDecoConstructor>
-      ) {
-        this.attachRegisterListener(fastify, service);
-        this.attachReadyListener(fastify, service);
-        this.attachCloseListener(fastify, service);
-      }
-
-      private attachRegisterListener(
-        fastify: FastifyInstance,
-        service: ConstructorReturnType<IServiceDecoConstructor>
-      ) {
-        const eventName = "onRegister";
-        fastify.addHook(eventName, async (fastifyInstance: FastifyInstance) =>
-          service[eventName]?.call(service, fastifyInstance)
-        );
-      }
-
-      private attachReadyListener(
-        fastify: FastifyInstance,
-        service: ConstructorReturnType<IServiceDecoConstructor>
-      ) {
-        const eventName = "onReady";
-        fastify.addHook(eventName, async () =>
-          service[eventName]?.call(service)
-        );
-      }
-
-      private attachCloseListener(
-        fastify: FastifyInstance,
-        service: ConstructorReturnType<IServiceDecoConstructor>
-      ) {
-        const eventName = "onClose";
-        fastify.addHook(eventName, async (fastifyInstance: FastifyInstance) =>
-          service[eventName]?.call(service, fastifyInstance)
-        );
       }
 
       private getInstanceFromContainer<T extends IClassWithKey<Constructor>>(
@@ -132,46 +134,6 @@ export function Module(config: IConfig) {
         Class: T
       ) {
         return container.get<ConstructorReturnType<T>>(Class.$KEY);
-      }
-
-      private getMergedContainer(
-        fastify: FastifyInstance,
-        includeController = false
-      ) {
-        return Container.merge(
-          Decorated.localContainer,
-          Decorated.exportContainer,
-          ...(includeController ? [Decorated.controllerContainer] : []),
-          ...this.getImportsContainers(fastify)
-        ) as Container;
-      }
-
-      private getImportsContainers(fastify: FastifyInstance) {
-        // For performance reasons, I think instead of trying to check if container is been created, we should remain the container with high confidence of being created and throw error if not created.
-
-        // Or somehow store the reference of the import containers in the CPU register for faster retrieval if possible or in RAM (physical or virtual) if user has large space
-        return _.map(config.imports, (importModule) =>
-          importModule.load(fastify)
-        );
-      }
-
-      private registerControllerHandler(
-        fastify: FastifyInstance,
-        controller: ConstructorReturnType<IControllerDecoConstructor>,
-        methodName: string
-      ) {
-        if (methodName === "$METADATA")
-          return throwError(
-            `methodName of controller ${controller} should not be used since it is a resevered key`
-          );
-        const handlerWithMetaData = controller[methodName];
-
-        return fastify.route({
-          ...handlerWithMetaData.$METADATA,
-          handler: this.getControllerHandlerFn(handlerWithMetaData),
-          url:
-            controller.$METADATA.basePath + handlerWithMetaData.$METADATA.url,
-        });
       }
 
       private getControllerHandlerFn(handlerWithMetaData: IController[string]) {
