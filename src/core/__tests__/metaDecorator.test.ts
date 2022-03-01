5e3caebc1b6b8c8d5f7ebf124c5ba9dcde8719de
VictorMadu <ebubevm@gmail.com>
2022-03-01 16:41:10 +0100

    Re-wrote metaDecorator and added test for it

---
+++ src/core/__tests__/metaDecorator.test.ts
@@ -0,0 +1,27 @@
+import { METADATA_KEY } from "../constant";
+import { IHandlerMetaData } from "../interface";
+import { metaDecorator } from "../metaDecorator";
+
+describe(`test for 'metaDecorator'`, () => {
+  test(`should assign 'metaKey' to 'metaValue' in a method's 'METADATA_KEY' prop`, () => {
+    const metaKey = "$$metaKey";
+    const metaValue = "$$metaValue";
+
+    const methodDecorator: any = metaDecorator(metaKey, metaValue);
+    const methodDeoratorForInitalMetaData = function (
+      target: Record<string | symbol, any>,
+      key: string | symbol
+    ) {
+      target[key][METADATA_KEY] = {};
+    };
+
+    class TestClass {
+      @methodDecorator
+      @methodDeoratorForInitalMetaData
+      methodA() {}
+    }
+
+    const mTestClass = new TestClass();
+    expect((mTestClass.methodA as any)[METADATA_KEY][metaKey]).toBe(metaValue);
+  });
+});
--- src/core/metaDecorator.ts
+++ src/core/metaDecorator.ts
@@ -1,14 +1,16 @@
+import { METADATA_KEY } from "./constant";
 import { IController, IHandlerMetaData } from "./interface";
 
-export function metaDecorator(
-  metaKey: string | symbol,
-  fn: (metaData: IHandlerMetaData) => void
-) {
+export function metaDecorator(metaKey: string | symbol, metaValue: any) {
   return function (
-    target: IController,
+    target: {
+      [key: string]: {
+        [METADATA_KEY]: Record<string | symbol, any>;
+      };
+    },
     key: string,
     descriptor: PropertyDescriptor
   ) {
-    fn(target[key]["$METADATA"]);
+    target[key]["$METADATA"][metaKey] = metaValue;
   };
 }
