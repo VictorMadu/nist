77ba2f1a662385b24a84a287b6f9eebe7b443664
VictorMadu <ebubevm@gmail.com>
2022-03-03 08:05:41 +0100

    Added test to be written for ControllerLoader

---
+++ src/core/__tests__/controller.loader.test.ts
@@ -0,0 +1,40 @@
+describe(`test for 'ControllerLoader'`, () => {
+  test.todo(`should be defined`);
+  test.todo(`should be of type 'function'`);
+  test.todo(`should be have method 'getInstance'`);
+  test.todo(`should be have method 'load'`);
+
+  describe(`test for method 'getInstance'`, () => {
+    test.todo(`should be defined`);
+    test.todo(`should be of type 'function'`);
+
+    describe(`test for calls`, () => {
+      describe(`test when with wrong argument`, () => {
+        test.todo(`should throw`);
+      });
+      describe(`test when with correct arguments`, () => {
+        test.todo(`should not throw`);
+        describe(`test for return value`, () => {
+          test.todo(`should be defined`);
+          test.todo(`should be instance of second argument`);
+        });
+      });
+    });
+  });
+
+  describe(`test for method 'load`, () => {
+    test.todo(`should be defined`);
+    test.todo(`should be of type 'function'`);
+
+    describe(`test for calls`, () => {
+      test.todo(`should not throw`);
+      describe(`test for 'ServiceAdapter'`, () => {
+        describe(`test for method 'attachLifeCycleListener'`, () => {
+          describe(`test for arguments passed`, () => {
+            test.todo(`should be when is passed'load'`);
+          });
+        });
+      });
+    });
+  });
+});
