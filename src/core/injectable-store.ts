ad70e192adc5a430e58f08a8451ea661e055ea2b
VictorMadu <ebubevm@gmail.com>
2022-03-27 13:54:31 +0100

    chore: Rename variable
    Renames injectableHandlers to injectableStore
    
    Better name.

--- src/core/injectable-store.ts
+++ src/core/injectable-store.ts
@@ -4,21 +4,18 @@ import { Constructor } from "../types";
 
 // Storing all injectable together and creates a handler for each one of them;
 export class InjectableStore {
-  private static injectableHandlers = new Map<
-    Constructor,
-    IInjectableHandler
-  >();
+  private static injectableStore = new Map<Constructor, IInjectableHandler>();
 
   static store(Injectable: Constructor) {
-    if (!this.injectableHandlers.has(Injectable))
-      this.injectableHandlers.set(Injectable, new InjectableHandler());
+    if (!this.injectableStore.has(Injectable))
+      this.injectableStore.set(Injectable, new InjectableHandler());
     return this;
   }
 
   static getInjectableHandler<T extends new (...args: any[]) => any>(
     Injectable: T
   ) {
-    return this.injectableHandlers.get(Injectable);
+    return this.injectableStore.get(Injectable);
   }
 }
 
