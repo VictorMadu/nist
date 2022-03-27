3c941a0ca8eed238974f14e86bbbdbf5f4e70987
VictorMadu <ebubevm@gmail.com>
2022-03-27 14:37:18 +0100

    feat: Added types
    This adds types to http param decorators to ensure that they stay 'stable' (sync) with what httpAdapter expects

--- src/fastify-adapter/http/http.param.decorator.ts
+++ src/fastify-adapter/http/http.param.decorator.ts
@@ -1,41 +1,37 @@
-import { FastifyReply, FastifyRequest } from "fastify";
 import { setParamMetadata } from "../../core/deco-utils";
+import { IMethodParamDecoFn } from "./interface/http.adapter.interface";
 // TODO: Check "src\fastify-adapter\ws\ws.param.decorator.ts:8" for better understanding. Add generic types for all setParaMetadata and setMethodMetadata. Even check other things
 
 export const dataKey = Symbol();
 
 export function Body() {
-  return setParamMetadata((req: FastifyRequest, rep: FastifyReply) => req.body);
+  return setParamMetadata<IMethodParamDecoFn>((req, rep) => req.body);
 }
 
 export function Params() {
-  return setParamMetadata(
-    (req: FastifyRequest, rep: FastifyReply) => req.params
-  );
+  return setParamMetadata<IMethodParamDecoFn>((req, rep) => req.params);
 }
 
 export function Query() {
-  return setParamMetadata(
-    (req: FastifyRequest, rep: FastifyReply) => req.query
-  );
+  return setParamMetadata<IMethodParamDecoFn>((req, rep) => req.query);
 }
 
 export function Req() {
-  return setParamMetadata((req: FastifyRequest, rep: FastifyReply) => req);
+  return setParamMetadata<IMethodParamDecoFn>((req, rep) => req);
 }
 
 export function Rep() {
-  return setParamMetadata((req: FastifyRequest, rep: FastifyReply) => rep);
+  return setParamMetadata<IMethodParamDecoFn>((req, rep) => rep);
 }
 
 export function ReqData() {
-  return setParamMetadata(
-    (req: FastifyRequest, rep: FastifyReply) => (req as any)[dataKey]
+  return setParamMetadata<IMethodParamDecoFn>(
+    (req, rep) => (req as any)[dataKey]
   );
 }
 
 export function RepData() {
-  return setParamMetadata(
-    (req: FastifyRequest, rep: FastifyReply) => (rep as any)[dataKey]
+  return setParamMetadata<IMethodParamDecoFn>(
+    (req, rep) => (rep as any)[dataKey]
   );
 }
