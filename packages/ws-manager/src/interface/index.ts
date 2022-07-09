5c6ea3b6b7170512f2b93e3ff5b85559b5af62e4
VictorMadu <ebubevm@gmail.com>
2022-07-09 16:15:57 +0100

    refactor(ws-manager): preparing for release

--- packages/ws-manager/src/interface/index.ts
+++ packages/ws-manager/src/interface/index.ts
@@ -4,17 +4,17 @@ import { Duplex } from "stream";
 import { OrWithPromise } from "ts-util-types";
 
 export type AuthAndGetUserDetails = (
-  req: IncomingMessage
+    req: IncomingMessage
 ) => OrWithPromise<Object | null | undefined>;
 
 export type WsHandler = (
-  wss: WebSocketServer,
-  ws: WebSocket,
-  req: IncomingMessage,
-  socket: Duplex,
-  head: Buffer,
-  data: {
-    userData: Object;
-    payload: any;
-  }
+    wss: WebSocketServer,
+    ws: WebSocket,
+    req: IncomingMessage,
+    socket: Duplex,
+    head: Buffer,
+    data: {
+        userData: Object;
+        payload: any;
+    }
 ) => any;
